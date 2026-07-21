7b01989043375ebbde3077b8b7344c8aaaefe917
Tony Cavella <9434191+acavella@users.noreply.github.com>
2026-07-20 20:14:11 -0400

    update license statement

--- estctl.sh
+++ estctl.sh
@@ -9,8 +9,7 @@
 #
 # Copyright (c) 2026 Tony Cavella <tony@cavella.com>
 #
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# This source code is licensed under the MIT license.
 #
 
 set -eo pipefail
